c49fd465b059232b2a609cab52cc92eff8d9a661
Vandal-William <vandal.william@hotmail.com>
2022-12-28 12:29:33 +0100

    Feat: drawGameMap function review

--- test/rpg.py
+++ test/rpg.py
@@ -1,8 +1,23 @@
 
+import json
 import sys
 from PySide6 import QtCore
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QIcon, Qt
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidget
+
+
+# Une class qui gère le statu du héro et les intéraction
+class Hero:
+
+    # Status du héro
+    pv = 100
+    mp = 100
+    force = 50
+    defense = 50
+
+    # Coordonné du héro sur la map
+    x = 5
+    y = 5
 
 
 class MainWindow(QMainWindow):
@@ -12,7 +27,6 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Empire Of Chaos")
         self.setMinimumSize(800, 600)
-        self.setMaximumSize(800, 600)
         self.setWindowIcon(QIcon("test/icons/rpg.png"))
 
         centralArea = QWidget()
@@ -20,81 +34,96 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(centralArea)
         centralArea.setStyleSheet("background: #419eee")
 
-        # Une class qui gère le statu du héro et les intéraction
-        class Hero:
+        def createCellInYPosition(widget):
 
-            # Status du héro
-            pv = 100
-            mp = 100
-            force = 50
-            defense = 50
+            # Cette fonction récupère en paramettre
+            # la ligne créer et y ajoute les cellules
+            # Une cellule est un QWidget
 
-            # Coordonné du héro sur la map
-            x = 51
-            y = 51
-
-            def move_right(map):
-                print("Move right")
-                # map.setCellWidget(Hero.y, Hero.x+1, Hero.hero)
-
-            def move_left(map):
-                map.setCellWidget(Hero.y, Hero.x-1, Hero.hero)
-
-            def move_top(map):
-                map.setCellWidget(Hero.y+1, Hero.x, Hero.hero)
-
-            def move_down(map):
-                map.setCellWidget(Hero.y-1, Hero.x, Hero.hero)
-
-            def attack():
-                pass
-
-            def talk():
-                pass
-
-        def currentYPosition(widget):
-            xPosition = 0
-            global x
-            global X
+            cell = []
             x = 0
-            while x < 13:
+            # permet de placer les cellules les une a coté des autres sur une ligne
+            xPosition = 0
+
+            for x in range(14):
+
                 X = QWidget(widget)
-                X.setGeometry(xPosition, 0, 125, 125)
+                X.setGeometry(xPosition, 0, 125, 123)
                 X.setStyleSheet("border: 1px solid black;")
-                xPosition = xPosition + 51
+                xPosition = xPosition + 49
                 x = x + 1
+                # Une fois la cellule créer elle est ajouter dans une liste appeler cell
+                cell.append(X)
+            # la fonction retourne la liste cell qui contient tout les QWidget créer pour une ligne donnée
+            return cell
+        global drawGameMap
 
-        def map():
+        def drawGameMap():
+            # permet de placer les lignes les une en dessous des autres
+            yPosition = 0
+            # compteur de boucle
+            y = 0
+            # un tableau qui contient le lignes de la map
+            row = []
+            # tableau bidimentionnel qui contien toute les coordonner de la map (QWidget)
+            global mapCell
+            mapCell = []
 
+            # borderMap définie les bordures de la map
             borderMap = QWidget(centralArea)
-            borderMap.setGeometry(0, 0, 800, 576)
+            borderMap.setGeometry(0, 0, 763, 565)
             borderMap.setStyleSheet(
                 "border: 1px solid black;" "margin: auto;")
 
-            yPosition = 0
-            y = 0
-            row = []
-
+            # dans borderMap je crée 10 ligne
             while y < 10:
 
                 Y = QWidget(borderMap)
-                Y.setGeometry(0, yPosition, 800, 125)
+                Y.setGeometry(0, yPosition, 763, 123)
                 Y.setStyleSheet("border: 1px solid black;")
-                yPosition = yPosition + 51
+                yPosition = yPosition + 49
                 y = y + 1
 
+                # chaque ligne créer est un QWidget que j'ajoute dans la liste row
                 row.append(Y)
-                for i in row:
-                    currentYPosition(i)
 
-                hero = QWidget(borderMap)
-                hero.setGeometry(Hero.x, Hero.y, 125, 124)
-                hero.setStyleSheet(
-                    "background: url(test/sprites/sprite/Hero.png)")
-        map()
+            for i in row:
+                # a chaque itération j'appel la fonction createCellInYPosition qui
+                # retourne la liste des QWidget créer pour la ligne i que je stock dans une variable cells
+                cells = createCellInYPosition(i)
+                # je stock la liste des QWidget pour la ligne i dans la liste mapCell
+                mapCell.append(cells)
+                # voila a quoi ressemblera mapCell : mapCell [ ligne1[QWidget1, QWidget2 ], ligne2[QWidget1, QWidget2 ], ... ]
+                # mapCell contient donc toute les coordonnée X et Y de la map
 
-    # def keyPressEvent(gameMap, event):
-    #     pass
+            global hero
+            # a l'aide de mapCell je place héro sur la map
+            hero = QWidget(mapCell[Hero.y][Hero.x])
+            hero.setGeometry(0, 0, 125, 124)
+            hero.setStyleSheet(
+                "background: url(test/sprites/sprite/Hero.png);" "border: none;")
+
+        drawGameMap()
+
+    def keyPressEvent(self, event):
+        x = 0
+        y = 0
+
+        print(event.key())
+        if event.key() == 16777236:
+            drawGameMap()
+            Hero.x = Hero.x + 1
+            print(Hero.x)
+            print("je vais a droite")
+
+        if event.key() == 16777234:
+            print("je vais a gauche")
+
+        if event.key() == 16777237:
+            print("je monte")
+
+        if event.key() == 16777235:
+            print("je decsent")
 
 
 if __name__ == "__main__":
