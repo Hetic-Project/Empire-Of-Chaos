04e99a2987b8c3a3bc71c7e143099e4bff4b1141
unknown <yalmanlucas@gmail.com>
2023-01-09 11:32:02 +0100

    .

--- test/functions/game_functions/addTextBox.py
+++ test/functions/game_functions/addTextBox.py
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-from PySide6.QtWidgets import QWidget, QTextEdit
-=======
 from PySide6.QtWidgets import QWidget, QLabel, QTextEdit, QLineEdit
 
->>>>>>> 89e3b18af19813387238b011a73bbece0c120c89
 
 def addTextBox(gameWindow, text):
     box = QWidget(gameWindow)
     box.setGeometry(35, 540, 690, 150)
     box.setStyleSheet(""" background : #ffffff; border-radius: 10px;""")
 
-<<<<<<< HEAD
-    message = QTextEdit(box)
-    message.setEnabled(False)
-    message.setText("{}".format(text))
-    message.setGeometry(10,10,700, 50)
-    message.setStyleSheet("color : green")
-=======
     message = QTextEdit("{}".format(text),box) 
     message.setGeometry(10, 20, 670, 70)
     message.setEnabled(False)
@@ -29,4 +18,3 @@ def addTextBox(gameWindow, text):
             font-size: 12px;
             background-attachment: scroll;
         """)
->>>>>>> 89e3b18af19813387238b011a73bbece0c120c89
